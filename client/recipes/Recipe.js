fb636b633d366db8d1b5f111078982984cf81d59
huybui <huythdnbkltv@gmail.com>
2019-09-22 22:05:29 +0300

    Toggle edit mode

--- client/recipes/Recipe.js
+++ client/recipes/Recipe.js
@@ -1,15 +1,17 @@
 Template.Recipe.events({
     'click .toggle-menu': function() {
         Meteor.call('toggleMenuItem', this._id, this.inMenu);
-    }
-});
+    },
 
-Template.Recipe.events({
     'click .fa-trash': function() {
         Meteor.call('deleteRecipe', this._id);
-    }
-});
+    },
 
+    'click .fa-pencil': function() {
+        let currentMode = Session.get('editMode');
+        Session.set('editMode', !currentMode);
+    },
+});
 
 Template.Recipe.helpers({
     updateRecipeId: function () {
